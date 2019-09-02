7b8fe3ff266376500c9b56b8b50d29626588343e
Daryk Kohler <daryk.kohler@gmail.com>
2019-09-02 13:43:14 -0700

    Initialized Cloudinary to handle photo storage

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -39,6 +39,7 @@ namespace DatingApp.API
             (Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
